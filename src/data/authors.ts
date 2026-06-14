0a0f2f4e484b16632028facb6dbdc5d33ca58348
driplase <d@j0.si>
2026-06-14 21:10:22 +0900

    new incident

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -8,4 +8,8 @@ export default [
     "name": "kouhei7497",
     "avatar": "https://cdn.discordapp.com/avatars/709605683028426794/3f22c5198d0f05c5bbf1c1fff9f58eff.png?size=64"
   },
+  {
+    "name": "driplase",
+    "avatar": "https://cdn.discordapp.com/avatars/959034815766728714/d4128bce2128b17492ad3e0e129754a1.png?size=64"
+  },
 ] as Author[]
