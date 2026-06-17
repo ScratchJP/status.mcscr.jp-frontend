1eb8d930527b80305a3ef25706c84e8f317585c7
driplase <d@j0.si>
2026-06-17 19:55:55 +0900

    feat: new incident again, this time due to earthquake chore: link highlightation in incident post

--- src/lib/incident-list.ts
+++ src/lib/incident-list.ts
@@ -2,4 +2,5 @@ export const incidentList = [
   "2026-02-15_16-35_sudden_death.mdx",
   "2026-02-25_22-43_port-broken.mdx",
   "2026-06-14_21-10_ip-address-unchecked.mdx",
+  "2026-06-17_20-00_earthquake-box-fall.mdx",
 ];
